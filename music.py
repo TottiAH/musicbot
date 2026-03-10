397c1f34c7ebcdea181da58c0eda8f1e4b1022c5
TottiAH <kumakichi11050716@icloud.com>
2026-03-10 22:41:33 +0900

    music.py

--- music.py
+++ music.py
@@ -2,21 +2,23 @@ import os
 import random
 import requests
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/1480898961744330803/XaOQtgiWgSepTzsQs_pP-zf69KAv0gtk5EMTFrptO8lh96_zHkqcNpcE8WHQdICohGW3"
+WEBHOOK_URL = "ここにDiscordWebhookURL"
 API_KEY = os.environ.get("YOUTUBE_API_KEY")
+
 
 def get_random_music():
     url = "https://www.googleapis.com/youtube/v3/search"
 
-   params = {
-    "part": "snippet",
-    "q": "hip hop chill music",
-    "type": "video",
-    "videoCategoryId": "10",
-    "maxResults": 25,
-    "regionCode": "US",
-    "key": API_KEY
-}
+    params = {
+        "part": "snippet",
+        "q": "hip hop chill music",
+        "type": "video",
+        "videoCategoryId": "10",
+        "maxResults": 25,
+        "regionCode": "US",
+        "key": API_KEY
+    }
+
     response = requests.get(url, params=params)
     print(response.status_code)
     print(response.text)
@@ -26,7 +28,6 @@ def get_random_music():
         return None
 
     data = response.json()
-
     items = data.get("items", [])
 
     if not items:
@@ -36,7 +37,6 @@ def get_random_music():
 
     title = video["snippet"]["title"]
     video_id = video["id"]["videoId"]
-
     video_url = f"https://www.youtube.com/watch?v={video_id}"
 
     return title, video_url
@@ -46,11 +46,9 @@ song = get_random_music()
 
 if song:
     title, url = song
-
     message = {
         "content": f"🎧 今日の音楽\n\n**{title}**\n{url}"
     }
-
 else:
     message = {
         "content": "音楽が見つからなかった"
