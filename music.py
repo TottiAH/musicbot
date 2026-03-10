5b3207eeff34adf11cc29f74100f0e7352b7f53c
TottiAH <kumakichi11050716@icloud.com>
2026-03-10 22:27:38 +0900

    Update music.py

--- music.py
+++ music.py
@@ -1,43 +1,53 @@
-import subprocess
+import os
+import random
 import requests
 
 WEBHOOK_URL = "https://discord.com/api/webhooks/1480898961744330803/XaOQtgiWgSepTzsQs_pP-zf69KAv0gtk5EMTFrptO8lh96_zHkqcNpcE8WHQdICohGW3"
+API_KEY = os.environ.get("YOUTUBE_API_KEY")
 
-def get_song():
-    cmd = [
-        "yt-dlp",
-        "ytsearch1:music",
-        "--print", "%(title)s",
-        "--print", "https://www.youtube.com/watch?v=%(id)s",
-        "--skip-download"
-    ]
+def get_random_music():
+    url = "https://www.googleapis.com/youtube/v3/search"
 
-    result = subprocess.run(cmd, capture_output=True, text=True)
+    params = {
+        "part": "snippet",
+        "q": "music",
+        "type": "video",
+        "maxResults": 25,
+        "key": API_KEY
+    }
 
-    if result.returncode != 0:
-        print("yt-dlp error:")
-        print(result.stderr)
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200:
+        print("YouTube API error")
         return None
 
-    lines = [line.strip() for line in result.stdout.splitlines() if line.strip()]
+    data = response.json()
 
-    if len(lines) < 2:
-        print("stdout was:")
-        print(result.stdout)
+    items = data.get("items", [])
+
+    if not items:
         return None
 
-    title = lines[0]
-    url = lines[1]
-    return title, url
+    video = random.choice(items)
+
+    title = video["snippet"]["title"]
+    video_id = video["id"]["videoId"]
+
+    video_url = f"https://www.youtube.com/watch?v={video_id}"
+
+    return title, video_url
 
 
-song = get_song()
+song = get_random_music()
 
 if song:
     title, url = song
+
     message = {
-        "content": f"🎵 今日の音楽\n**{title}**\n{url}"
+        "content": f"🎧 今日の音楽\n\n**{title}**\n{url}"
     }
+
 else:
     message = {
         "content": "音楽が見つからなかった"
